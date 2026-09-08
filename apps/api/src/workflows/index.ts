f53b57aeacc1a317568f2c4f99591350b03bc674
shaoxiaoyu-D <2061864083@qq.com>
2026-09-08 19:31:16 +0800

    feat(api): add project write access workflow orchestration (#48)

---
+++ apps/api/src/workflows/index.ts
@@ -0,0 +1,11 @@
+export { ProjectWriteAccessWorkflow } from "./project-write-access.workflow.js";
+export {
+  type CheckFeatureForWriteInput,
+  type CheckModuleForWriteInput,
+  type FeatureWriteAccessFailure,
+  type FeatureWriteAccessResult,
+  type ModuleWriteAccessFailure,
+  type ModuleWriteAccessResult,
+  type ProjectWriteAccessStep,
+} from "./project-write-access.workflow.js";
+export { WorkflowModule } from "./workflow.module.js";
